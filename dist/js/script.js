91922b0a8f1aed92c451389c72c2594056096e32
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-10-27 02:35:46 +0300

    accordion done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,66 @@
-const hamburger = document.querySelector('.hamburger'),
-        menu = document.querySelector('.menu'),
-        closeElem = document.querySelector('.menu__close');
+// nav
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active')
+const navItem = document.querySelectorAll('.nav__link');
+navItem.forEach(item => {
+    // item.classList.remove('active-link');
+    item.addEventListener('click', () => {
+        navItem.forEach(item => {
+            item.classList.remove('active-link');
+        });
+        item.classList.add('active-link');
+    });
 });
 
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active')
-});
 
-const counters = document.querySelectorAll('.skills__ratings-counter'),
-    lines = document.querySelectorAll('.skills__ratings-line span');
+const accordion = () => {
+    const btn = document.querySelector('.nav__toggle');
+    const appsIcon = document.querySelector('.uil-apps');
 
-counters.forEach( (item, i) => {
-    lines[i].style.width = item.innerHTML
-});
+    btn.addEventListener('click', function() {
+        if(!btn.classList.contains('active-style')) {
+            btn.classList.remove('active-style');
+            btn.previousElementSibling.classList.remove('active-content');
+            btn.previousElementSibling.style.minHeight = '0px';
+        }
+
+        btn.classList.toggle('active-style');
+        btn.previousElementSibling .classList.toggle('active-content');
+
+        if(btn.classList.contains('active-style')) {
+            btn.previousElementSibling.style.minHeight = 150 + 'px';
+            appsIcon.classList.toggle('uil-times');
+
+            // btn.previousElementSibling.style.opacity = 1;
+
+        } else {
+            btn.previousElementSibling.style.minHeight = '0px';
+            btn.previousElementSibling.style.paddingBottom = '0px';
+            appsIcon.classList.toggle('uil-times');
+
+
+        }
+    });
+};
+
+accordion();
+
+
+
+// const hamburger = document.querySelector('.hamburger'),
+//         menu = document.querySelector('.menu'),
+//         closeElem = document.querySelector('.menu__close');
+
+// hamburger.addEventListener('click', () => {
+//     menu.classList.add('active')
+// });
+
+// closeElem.addEventListener('click', () => {
+//     menu.classList.remove('active')
+// });
+
+// const counters = document.querySelectorAll('.skills__ratings-counter'),
+//     lines = document.querySelectorAll('.skills__ratings-line span');
+
+// counters.forEach( (item, i) => {
+//     lines[i].style.width = item.innerHTML
+// });
